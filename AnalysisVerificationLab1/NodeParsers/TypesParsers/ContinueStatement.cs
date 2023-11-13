41d73258c1cc3e6925e12aef515b92b5e5fe7fb4
sergeyfedorov02 <sergey_fedorov200001@mail.ru>
2023-11-13 19:40:30 +0300

    fixed continue

---
+++ AnalysisVerificationLab1/NodeParsers/TypesParsers/ContinueStatement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AnalysisVerificationLab1.NodeParsers.TypesParsers
+{
+    public class ContinueStatement
+    {
+        private string _continueStatementParserResult;
+
+        public string GetContinueStatementParserResult(ContinueStatementSyntax node)
+        {
+            _continueStatementParserResult = ContinueStatementParser(node);
+            return _continueStatementParserResult;
+        }
+
+        private static string ContinueStatementParser(ContinueStatementSyntax node)
+        {
+            var result = node.ContinueKeyword.ValueText;
+            return $"{result};";
+        }
+    }
+}
